cde72dd44a7ace7bbc8e3f1b97d12ba828720b04
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-05-31 18:03:51 -0500

    Actualización TodosApp

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -23,19 +23,24 @@ class Todo(BaseModel):
     complete:bool
 
 @app.post("/")
-async def create_todo(todo:Todo, db:Session = Depends(getDB)):
+async def create_todo(todo:Todo, user:dict=Depends(get_current_user), db:Session = Depends(getDB)):
+    if user is None:
+        raise get_user_exception()
     todo_model=models.Todos()
     todo_model.title=todo.title
     todo_model.description=todo.description
     todo_model.priority=todo.priority
     todo_model.complete=todo.complete
+    todo_model.owner_id=user.get("id")
     db.add(todo_model)
     db.commit()
     return successful_response(201)
         
 @app.put("/{todo_id}")
-async def update_todo(todo_id:int,todo:Todo,db:Session=Depends(getDB)):
-    todo_model=db.query(models.Todos).filter(models.Todos.id==todo_id).first()
+async def update_todo(todo_id:int,todo:Todo,user:dict=Depends(get_current_user),db:Session=Depends(getDB)):
+    if user is None:
+        raise get_user_exception()
+    todo_model=db.query(models.Todos).filter(models.Todos.id==todo_id).filter(models.Todos.owner_id==user.get("id")).first()
     if todo_model is None:
         raise HttpException()
     todo_model.title=todo.title
@@ -67,8 +72,10 @@ async def read_all_by_user(user:dict=Depends(get_current_user), db:Session=Depen
     return db.query(models.Todos).filter(models.Todos.owner_id==user.get("id")).all()
 
 @app.get("/toto/{todo_id}")
-async def read_todo(todo_id:int,db:Session=Depends(getDB)):
-    todo_model=db.query(models.Todos).filter(models.Todos.id==todo_id).first()
+async def read_todo(todo_id:int,user:dict=Depends(get_current_user),db:Session=Depends(getDB)):
+    if user is None:
+        raise get_user_exception()
+    todo_model=db.query(models.Todos).filter(models.Todos.id==todo_id).filter(models.Todos.owner_id==user.get("id")).first()
     if todo_model is not None:
         return todo_model
     raise HttpException()
